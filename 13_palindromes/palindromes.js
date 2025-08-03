3cdba2f221f9723cc988f1d73306776d4f9c1112
junhanyong <jhyong08@gmail.com>
2025-08-03 14:52:43 +0800

    Completed 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'; 
+    const cleanString = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
 
-};
+        .join('')
+    const reverseString = cleanString.split('').reverse().join('')
+    return reverseString === cleanString
+}
 
 // Do not edit below this line
 module.exports = palindromes;
