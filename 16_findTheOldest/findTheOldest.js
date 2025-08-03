34e2b3bab8bf525ef810cd9a332e0aa0b0209437
junhanyong <jhyong08@gmail.com>
2025-08-03 20:40:51 +0800

    Completed 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const d = new Date()
+    const currentYear = d.getFullYear();
 
+    const ageArray = arr.map((element) => {
+        if (element.yearOfDeath == undefined){
+            return currentYear - element.yearOfBirth
+        }
+        else {
+            return element.yearOfDeath - element.yearOfBirth
+        }
+    })
+    const max = ageArray.reduce((a, b) => a > b ? a : b)
+    return arr[ageArray.indexOf(max)]
 };
 
 // Do not edit below this line
