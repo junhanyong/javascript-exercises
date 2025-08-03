ef31cacea6b6dd73356c62a6879d379c39f61fba
junhanyong <jhyong08@gmail.com>
2025-08-03 20:16:45 +0800

    Completed 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const mappedArr = arr.map((element) => element.title)
+    return mappedArr
 };
 
 // Do not edit below this line
